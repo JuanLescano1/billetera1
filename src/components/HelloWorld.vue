8cef6731619addd87924a7af4acba4f3394c11c9
Juan Cruz Lescano <juancruzlescano@hotmail.com>
2024-10-21 17:33:54 -0300

    RouterYVista

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -7,19 +7,28 @@
     <div v-if="monedaSeleccionada === 'btc' && getBtcData">
       <h2>Datos BTC</h2>
       <p v-for="(exchange, name) in getBtcData" :key="name">
-        {{ name }}: Ask - {{ exchange.ask }}
+        Exchange: {{ name }} - Precio de compra: {{ exchange.ask }} - Precio de
+        compra con comisiones:
+        {{ exchange.totalAsk }}
+        <button @click="compraCripto('btc', name)">Compra</button>
       </p>
     </div>
     <div v-if="monedaSeleccionada === 'eth' && getEthData">
       <h2>Datos ETH</h2>
       <p v-for="(exchange, name) in getEthData" :key="name">
-        {{ name }}: Ask - {{ exchange.ask }}
+        Exchange: {{ name }} - Precio de compra: {{ exchange.ask }} - Precio de
+        compra con comisiones:
+        {{ exchange.totalAsk }}
+        <button @click="compraCripto('eth', name)">Compra</button>
       </p>
     </div>
     <div v-if="monedaSeleccionada === 'usdt' && getUsdtData">
       <h2>Datos USDT</h2>
       <p v-for="(exchange, name) in getUsdtData" :key="name">
-        {{ name }}: Ask - {{ exchange.ask }}
+        Exchange: {{ name }} - Precio de compra: {{ exchange.ask }} - Precio de
+        compra con comisiones:
+        {{ exchange.totalAsk }}
+        <button @click="compraCripto('usdt', name)">Compra</button>
       </p>
     </div>
   </div>
@@ -43,6 +52,9 @@ export default {
     ]),
   },
   methods: {
+    compraCripto(moneda, exchange) {
+      this.$router.push({ name: "compra", params: { moneda, exchange } });
+    },
     cargaDatos() {
       this.$store.dispatch("consultaApi");
     },
